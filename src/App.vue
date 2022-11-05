4fd786db10ec7967efaa3ec6d1bde14b0dd4ebda
mintmintra <mintracss019@gmail.com>
2022-11-06 02:11:02 +0700

    vue basic

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import MyBar from './components/MyBar.vue';
+import MyCard from './components/MyCard.vue';
 </script>
 
 <template>
@@ -8,10 +9,40 @@ import MyBar from './components/MyBar.vue';
   </header>
 
   <main>
-    
+    <h1 style="padding-left: 10px;">Attractions</h1>
+    <div class="grid-container">
+      <MyCard v-for="item in items" v-bind="item" />
+    </div>
   </main>
 </template>
 
-<style scoped>
+<script>
+export default {
+  data() {
+    return {
+      items: []
+    }
+  },
+  created() {
+    fetch('https://www.mecallapi.com/api/attractions/')
+    .then(res => res.json())
+    .then((result) => {
+      this.items = result
+    })
+  }
+}
+</script>
 
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-column-gap: 0.5rem;
+  grid-row-gap: 0.5rem;
+}
+@media (min-width: 576px) {
+  .grid-container { grid-template-columns: repeat(1, 1fr); }
+}
+@media (min-width: 992px) {
+  .grid-container { grid-template-columns: repeat(3, 1fr); }
+}
 </style>
