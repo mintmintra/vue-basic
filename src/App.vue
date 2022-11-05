07dbe6404c48276446781b9a703da43272eb470f
mintmintra <mintracss019@gmail.com>
2022-11-05 22:50:30 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import MyBar from './components/MyBar.vue';
+</script>
+
+<template>
+  <header>
+    <MyBar />
+  </header>
+
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+
+</style>
